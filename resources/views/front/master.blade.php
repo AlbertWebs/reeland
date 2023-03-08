ab30368f6d4f3eb667604b6aac3aa69a866d50f5
Albert Muhatia <albertmuhatia@gmail.com>
2023-03-08 10:49:55 +0300

    Search Bar

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -144,18 +144,8 @@
       </div>
       <div class="col-md-4 col-sm-6">
         <div class="single-query form-group">
-          <div class="intro">
-            <select>
-              <option selected="" value="any">Location</option>
-              <option>All areas</option>
-              <option>Bayonne </option>
-              <option>Greenville</option>
-              <option>Manhattan</option>
-              <option>Queens</option>
-              <option>The Heights</option>
-            </select>
+            <input type="text" name="location" class="keyword-input" placeholder="e.g Loresho">
           </div>
-        </div>
       </div>
       <div class="col-md-2 col-sm-12 text-right">
         <button type="button" class="advanced text-center text-uppercase border_radius"><i class="icon-settings"></i>advanced</button>
@@ -168,12 +158,14 @@
             <div class="intro">
               <select>
                 <option class="active">Property Type</option>
-                <option>All areas</option>
-                <option>Bayonne </option>
-                <option>Greenville</option>
-                <option>Manhattan</option>
-                <option>Queens</option>
-                <option>The Heights</option>
+                <option selected="selected" disabled>Select Type</option>
+                <?php
+                    $Category = DB::table('categories')->get();
+                ?>
+                @foreach($Category as $Cat)
+                <option value="{{$Cat->id}}">{{$Cat->title}} </option>
+                @endforeach
+
               </select>
             </div>
           </div>
@@ -184,90 +176,32 @@
               <select>
                 <option class="active">Property Status</option>
                 <option>All areas</option>
-                <option>Bayonne </option>
-                <option>Greenville</option>
-                <option>Manhattan</option>
-                <option>Queens</option>
-                <option>The Heights</option>
+                <option>Sale </option>
+                <option>Rent</option>
               </select>
             </div>
           </div>
         </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="row search-2">
-            <div class="col-md-6 col-sm-6">
-              <div class="single-query form-group">
-                <div class="intro">
-                  <select>
-                    <option class="active">Min Beds</option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6 col-sm-6">
-              <div class="single-query form-group">
-                <div class="intro">
-                  <select>
-                    <option class="active">Min Baths</option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3 col-sm-6">
+
+        <div class="col-md-6 col-sm-6">
           <div class="row">
             <div class="col-sm-6">
               <div class="single-query form-group">
-                <input type="text" class="keyword-input" placeholder="Min Area (sq ft)">
+                <input type="text" class="keyword-input" placeholder="Min Price">
               </div>
             </div>
             <div class="col-sm-6">
               <div class="single-query form-group">
-                <input type="text" class="keyword-input" placeholder="Max Area (sq ft)">
+                <input type="text" class="keyword-input" placeholder="Max Price">
               </div>
             </div>
           </div>
         </div>
         <div class="col-md-4 col-sm-5">
-          <div class="group-button-search">
-            <a data-toggle="collapse" href=".html" class="more-filter">
-              <i class="fa fa-plus text-1" aria-hidden="true"></i><i class="fa fa-minus text-2 hide" aria-hidden="true"></i>
-              <div class="text-1">Show more search options</div>
-              <div class="text-2 hide">less more search options</div>
-            </a>
-          </div>
+
         </div>
         <div class="col-md-8">
           <div class="row">
-            <div class="col-md-8 col-sm-6">
-              <div class="single-query-slider">
-                <label>Price Range:</label>
-                <div class="price text-right">
-                  <span>$</span>
-                  <div class="leftLabel"></div>
-                  <span>to $</span>
-                  <div class="rightLabel"></div>
-                </div>
-                <div data-range_min="0" data-range_max="1500000" data-cur_min="0" data-cur_max="1500000" class="nstSlider">
-                  <div class="bar"></div>
-                  <div class="leftGrip"></div>
-                  <div class="rightGrip"></div>
-                </div>
-              </div>
-            </div>
             <div class="col-md-4 text-right form-group">
               <button type="submit" class="border_radius top15 btn-yellow bottom15">Search property</button>
             </div>

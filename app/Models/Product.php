c9fbdf8e12775afbf33b595958adef7d4b2c4db9
Albert Muhatia <albertmuhatia@gmail.com>
2023-03-09 09:02:07 +0300

    Update SKU

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use BinaryCats\Sku\HasSku;
+use BinaryCats\Sku\Concerns\SkuOptions;
 
 class Product extends Model
 {
